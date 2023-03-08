e80cc1507eab89c42388079042ce4ec87a309768
MrLegendDragon <127273057+MrLegendDragon@users.noreply.github.com>
2023-03-08 14:25:36 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: '5618361858:AAERHhB_lkEnNw1moKs3M5e0HGmLRRAYUmA',
+    token: '6115940291:AAHSaKB0lqcEWa6xHkIZcvALdefeU9wzW58',
     admins: [5828714023,1856249311],
     curr:'Paisa'
 }
